28079edb1dfe0d4e9e8e5ecdcdfbc15ba96319d1
blackrebel <72fabio1@virgilio.it>
2017-04-19 13:38:16 +0200

    MediatekHacks: added VectorImpl symbols for JB blobs
    Change-Id: I9929a59eb170b2ea35ef8df17c84961e39dae1b5

--- libutils/MediatekHacks.cpp
+++ libutils/MediatekHacks.cpp
@@ -1,6 +1,22 @@
 extern "C" {
     void _ZN7android6Tracer4initEv(){}
     void _ZNK7android9CallStack4dumpEPKc(){}
+    void _ZN7android10VectorImpl19reservedVectorImpl1Ev() { };
+    void _ZN7android10VectorImpl19reservedVectorImpl2Ev() { };
+    void _ZN7android10VectorImpl19reservedVectorImpl3Ev() { };
+    void _ZN7android10VectorImpl19reservedVectorImpl4Ev() { };
+    void _ZN7android10VectorImpl19reservedVectorImpl5Ev() { };
+    void _ZN7android10VectorImpl19reservedVectorImpl6Ev() { };
+    void _ZN7android10VectorImpl19reservedVectorImpl7Ev() { };
+    void _ZN7android10VectorImpl19reservedVectorImpl8Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl1Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl2Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl3Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl4Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl5Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl6Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl7Ev() { };
+    void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl8Ev() { };
     volatile int _ZN7android6Tracer8sIsReadyE = 1;
     unsigned long long _ZN7android6Tracer12sEnabledTagsE = 0;
     int _ZN7android6Tracer8sTraceFDE = -1;
